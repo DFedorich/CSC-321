371506dce8904e3c342c4ea5f0d8c9c8f25a606d
DFedorich <53987992+DFedorich@users.noreply.github.com>
2020-01-29 12:00:28 -0500

    Add files via upload

---
+++ ODarwish.java
@@ -0,0 +1,78 @@
+
+public class ODarwish {
+
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			System.out.println("Hello");
+			System.out.println("Drew Fedorich");
+			System.out.println(74+36);
+			System.out.println(50/3);
+			System.out.println(-5+8*6);
+			System.out.println((55+9) % 9 );
+			System.out.println(20+-3*5/8);	
+			System.out.println( 20 + -3*5 / 8 );
+			System.out.println(5 + 15 / 3 * 2 - 8 % 3 );
+
+			System.out.println(25*5);
+			
+			
+		System.out.println(125 + " + " + 24 + " = " + 
+			  (125 + 24));
+			   
+		System.out.println(125 + " - " + 24 + " = " + 
+			  (125 - 24));
+			   
+		System.out.println(125 + " x " + 24 + " = " + 
+			  (125 * 24));
+			   
+		System.out.println(125 + " / " + 24 + " = " + 
+			  (125 / 24));
+			 
+		System.out.println(125 + " % " + 24 + " = " + 
+			  (125 % 24));
+			
+			
+			  
+		System.out.println(25*5);
+
+		System.out.println(125+24);
+		System.out.println(125-24);
+		System.out.println(125*24);
+		System.out.println(125/24);
+		System.out.println(125%24);
+		
+		
+		System.out.println(8*1);
+		System.out.println(8*2);
+		System.out.println(8*3);
+		System.out.println(8*4);
+		System.out.println(8*5);
+		System.out.println(8*6);
+		System.out.println(8*7);
+		System.out.println(8*8);
+		System.out.println(8*9);
+		System.out.println(8*10);
+
+		System.out.println((25.5*3.5-3.5*3.5)/(40.5-4.5));
+		
+		System.out.println(4.0*(1-(1.0/3)+(1.0/5)-(1.0/7)+(1.0/9)-(1.0/11)));
+		
+		System.out.println("* * * * * * ==================================                          \r\n" + 
+				" * * * * *  ==================================                          \r\n" + 
+				"* * * * * * ==================================                          \r\n" + 
+				" * * * * *  ==================================                          \r\n" + 
+				"* * * * * * ==================================                          \r\n" + 
+				" * * * * *  ==================================                          \r\n" + 
+				"* * * * * * ==================================                          \r\n" + 
+				" * * * * *  ==================================                          \r\n" + 
+				"* * * * * * ==================================                          \r\n" + 
+				"==============================================                          \r\n" + 
+				"==============================================                          \r\n" + 
+				"==============================================                          \r\n" + 
+				"==============================================                          \r\n" + 
+				"==============================================                          \r\n" + 
+				"==============================================");
+	}
+
+}
