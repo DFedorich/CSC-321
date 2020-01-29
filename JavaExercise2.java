620abaf7728be1f6edd507b18499696f67ce657b
DFedorich <53987992+DFedorich@users.noreply.github.com>
2020-01-29 12:06:52 -0500

    Add files via upload
    1/29/2020

--- JavaExercise2.java
+++ JavaExercise2.java
@@ -19,6 +19,27 @@ if (x%p == 0)
 	return( y/p - x/p + 1);
 return(y/p - x/p);
 
+
+{
+    Scanner in = new Scanner(System.in);
+    System.out.print("Input the first number : ");
+    int a = in.nextInt();  
+	System.out.print("Input the second number: ");
+	int b = in.nextInt(); 
+	System.out.println("Result: "+result(a, b));
+}
+public static int result(int x, int y)
+ {  
+if(x == y)
+	return 0;
+    if(x % 6 == y % 6)
+	    return (x < y) ? x : y;
+    return (x > y) ? x : y;
+ }
+
+
+
+
 }
 
 }
