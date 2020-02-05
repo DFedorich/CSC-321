590d31ee6437474f30e3d2fed0ef8e513863fed0
DFedorich <53987992+DFedorich@users.noreply.github.com>
2020-02-05 11:30:55 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,13 +1,20 @@
-
 public class Main {
 
 	public static void main(String[] args) {
 		Student obj= new Student();
-		obj.Set("123", "Chris", "LS");
+		obj.Set(123, "Chris", "LS");
 		
-		System.out.println(obj.getID());
+		System.out.println(obj.getId());
 		System.out.println(obj.getMajor());
 		System.out.println(obj.getname());
+		
+		Student obj2=new Student();
+		obj2.Set(124,  "Ben",  "CS");
+		
+		System.out.println(obj.getId());
+		System.out.println(obj.getMajor());
+		System.out.println(obj.getname());
+		
 
 	}
 
